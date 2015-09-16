d88f0d2e767e88c40221b3f0aee8ecd20e64e624
Alex McLean <alexmclean206@berkeley.edu>
2015-09-16 09:15:16 -0700

    elements by class now matches document.getElementsByClass

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,29 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+	var requestedElements = [];
+	var getElems = function(nodes){
+		if(nodes){
+			if(nodes.length){
+				for(var i = 0; i < nodes.length; i++) {
+					if(nodes[i].classList !== undefined) {
+						getElems(nodes[i]);
+					}
+				}
+			} else {   //perform search on child nodes
+				var classes = nodes.classList;
+				if(classes){
+					if(classes.contains(className)) {
+						requestedElements.push(nodes);
+					}
+					getElems(nodes.childNodes)
+				}
+			}
+		}
+	};
+	getElems(document.body);
+	//console.log(requestedElements.toString());
+	//console.log(Array.prototype.slice.apply(document.getElementsByClassName('targetClassName')).toString());
+	return requestedElements;
 };
